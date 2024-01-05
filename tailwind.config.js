4694ba852fab747a8cfd0a12cfdad2dc6d8b4f4f
ardigiandi <ardiansyahg9080@gmail.com>
2024-01-06 02:08:56 +0700

    slicing web

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        hijau: '#3C9379'
+      },
+    },
+  },
+  plugins: [],
+}
+
